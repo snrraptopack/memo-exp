62d739e226c8c9587b5cbe191db9a7f9572124ed
Amihere Theophilus Junior <snrraptopack@gmail.com>
2026-08-25 09:20:51 +0000

    chore: drop temporary debug artifacts from scratch

--- scratch/ws-main.js
+++
@@ -1,7 +0,0 @@
-import * as _MD from "@memoized-dom/runtime";
-import { mount } from '@memoized-dom/runtime';
-import { installWorkspaceApi } from './api';
-import { WorkspaceApp } from './WorkspaceApp';
-import './styles.css';
-installWorkspaceApi();
-mount('root', WorkspaceApp);
